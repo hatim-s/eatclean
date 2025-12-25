fc2225f33ca64740582b7977597ff336800a98a3
Hatim Shakir <hatimshakir9179@gmail.com>
2025-12-26 00:47:46 +0530

    feat(auth): integrate GitHub login functionality and update dependencies
    - Added GitHub login button to the sign-in page.
    - Enhanced LoginForm component to accept dynamic headers and descriptions.
    - Updated dependencies in package.json and bun.lock for @base-ui/react and related packages.

--- app/(auth)/sign-in/sign-in.tsx
+++ app/(auth)/sign-in/sign-in.tsx
@@ -1,3 +1,22 @@
+'use client';
+
+import { Button } from "@/ui/components/base/button";
+import { LoginForm } from "@/ui/components/login-form";
+import { GithubIcon } from "lucide-react";
+import { signIn } from "@/auth/client";
+
 export default function SignInPage() {
-    return <div>Sign In</div>
+  return <div className="flex flex-col items-center justify-center h-screen">
+    <LoginForm className="max-w-1/3 min-w-[500px]" formHeader="Login" formDescription="Login with your GitHub account">
+      <Button variant="default" type="button" onClick={async () => {
+        await signIn.social({
+          provider: "github",
+          callbackURL: "/",
+        });
+      }}>
+        <GithubIcon className="me-2" />
+        Login with GitHub
+      </Button>
+    </LoginForm>
+  </div>;
 }
