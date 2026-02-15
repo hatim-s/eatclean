75e5b8651dc02f4d197626f2c5ebf628805c1d43
Hatim Shakir <hatimshakir9179@gmail.com>
2026-02-16 00:27:36 +0530

    config: Update Drizzle migration output directory from './drizzle' to './db/migrations'.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,3 @@
-
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
@@ -6,7 +5,7 @@ config({ path: '.env' });
 
 export default defineConfig({
     schema: './db/schema.ts',
-    out: './drizzle',
+    out: './db/migrations',
     dialect: 'turso',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
